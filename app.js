a158c1d715af22c0747da83c15529b48ef508495
MahnoorAsim-123 <mahnoorasim423@gmail.com>
2022-03-21 23:31:29 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,27 @@
+function login(){
+
+    alert("Successfull Login");
+}
+
+function  register(){
+
+    alert("Successfull Register");
+}
+
+function booking(){
+    alert("Booking Confirmed");
+}
+
+function contct_btn(){
+alert("Your message has been sent");
+}
+
+
+function seeMore(){
+    var seemore = document.getElementById('see');
+    var btn = document.getElementById("seebtn");
+    btn.className = "hide";
+    seemore.className = "show";
+
+
+}
